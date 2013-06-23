02941fe648cc313503c0bb05ca3ecd97bed03b1d
nombre5ans <lewishw@uab.edu>
2013-06-23 05:39:05 +0000

    Initial commit after ignoring database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'ca4eb340a31418934b53d5701fd2b70afe231ecceac32f0c0fc2664d32ef653a2a29e377201611eb2e50d3571d239545824e0ca4f0b7d4a15b70219112a11cee'
